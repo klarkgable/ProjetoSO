e719747d31f46a7d6f4d0d540949793d971ec769
klarkgable <klark_94_123@hotmail.com>
2018-05-29 03:16:45 -0300

    Update Estrutura.h

--- Estrutura.h
+++ Estrutura.h
@@ -1,39 +1,42 @@
 #ifndef ESTRUTURA_H_INCLUDED
 #define ESTRUTURA_H_INCLUDED
 
-/// Error macros
-#define SUCCESS 0 /// Global Success
-#define ERROR_ARGS -1 /// Global Argument Error
-#define ERROR_FUNC -2 /// Global Functionality Error
-#define ERROR_SHM 1 /// Shared Memory Error
-#define ERROR_MSQ 2 /// Message Queue Error
+/// Erros
+#define SUCCESS 0
+#define ERROR_ARGS -1
+#define ERROR_FUNC -2
+#define ERROR_SHM 1
+#define ERROR_MSQ 2
 
-/// IPC macros
-#define SHM_FLAGS 0644 /// Shared Memory Flag
-#define SHM_KEY 0x111141 /// Shared Memory Key
-#define MSQ_FLAGS 0644 /// Message Queue Flag
-#define MSQ_KEY 0x159560 /// Message Queue Key
+/// IPCs
+#define SHM_FLAGS 0644 /// flag de memoria
+#define SHM_KEY 0x111141 /// chave de memoria
+#define MSQ_FLAGS 0644 /// Flag de mensagem
+#define MSQ_KEY 0x159560 /// chave de mensagem
 #define MSQ_KEY2 0x159561
 #define MSG_TYPE 1
-#define SHM_KEY2 0x111142 /// Second Message Queue Key
+#define SHM_KEY2 0x111142
 
-/// Messages
-#define MSG_CANCEL "Cancel" /// Message to cancel a job
-#define MSG_LIST "List" /// Message to request the jobs list
-#define MSG_SHUTDOWN "Shutdown" /// Message to shutdown the server
+/// Mensagens
+#define MSG_CANCEL "Cancel" /// Mensagem para cancelar um processo
+#define MSG_LIST "List" /// Mensagem para requerer lista de processos
+#define MSG_SHUTDOWN "Shutdown" /// Mensagem para shutdown
 
-typedef struct job {
-    short _jobId; /// Unique
-    std::string _programName; /// Path
-    int _hour;
-    int _minute;
-    int _times;
-} Job;
+typedef struct processo {
+    short _processoId; ///identificador unico para tupla
+    std::string _nomePrograma;
+    int _hora;
+    int _minutos;
+    int _copias;
+    int pri; ///prioridade do processo(1>2>3...)
 
-typedef struct jobmsg {
-	long _mtype;
-	char _job[100];
-} JobMessage;
+} Processo;
+
+
+typedef struct msgProcesso {
+	long _mtipo;
+	char _processo[100];
+} MsgProcesso;
 
 
 #endif // ESTRUTURA_H_INCLUDED
